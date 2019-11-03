30c6f3aa344b1a6c42987b309fb896f50e43655b
Cesar David Mejia Sac <cesarsac@uotlook.com>
2019-11-03 10:41:45 -0600

    Se le agrego css

--- Django_Crud_Project/CRUD/views.py
+++ Django_Crud_Project/CRUD/views.py
@@ -3,4 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views her
+def index(request):
+    return render(request, 'index.htme')
+
